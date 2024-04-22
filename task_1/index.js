a6ef13fcc152fe650db3c21b14e9e2d943c44b44
Ivan Antonov <ivan1antonov@gmail.com>
2024-04-22 10:03:57 +0400

    task 1 solution

--- task_1/index.js
+++ task_1/index.js
@@ -3,6 +3,24 @@ import {encoded, translations} from './data.js'
 console.log("Let's rock")
 console.log(encoded, translations)
 
+function decode(encoded, translations) {
+  const excludeFields = ['groupId', 'service', 'formatSize', 'ca'];
+  const uniqueIds = new Set();
 
+  return encoded.map(item => {
+      let newItem = {};
+      for (let key in item) {
+          if (excludeFields.includes(key)) {
+              newItem[key] = item[key];
+          } else if (key.endsWith('Id') && item[key] in translations) {
+              uniqueIds.add(item[key]);
+              newItem[key] = translations[item[key]];
+          } else {
+              newItem[key] = item[key];
+          }
+      }
+      return newItem;
+  }), Array.from(uniqueIds);
+}
 
 // console.log(decoded)
